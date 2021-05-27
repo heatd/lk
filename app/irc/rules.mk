bc5e681e97c40ea68008cea8b5a355198fb15243
Travis Geiselbrecht <travisg@gmail.com>
2021-05-27 03:14:19 -0700

    WIP irc app

--- app/irc/rules.mk
+++ app/irc/rules.mk
@@ -3,9 +3,10 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 MODULE := $(LOCAL_DIR)
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/irc.c \
+	$(LOCAL_DIR)/irc.cpp \
 
 MODULE_DEPS := \
+    lib/libcpp \
     lib/minip
 
 include make/module.mk
